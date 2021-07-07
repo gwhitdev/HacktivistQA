b7a51131a25a286cb320d9d24dbbff188c935ce1
Gareth Whitley <me@garethwhitley.online>
2021-07-07 18:08:46 +0100

    updated controllers

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Answer;
 
 class AnswersController extends Controller
 {
-    //
+    public function add(Request $request)
+    {
+        $request->validateWithBag('answer', [
+            'answer_text' => 'required|min:5',
+        ],[
+            'answer_text.required' => 'You must write something to provide an answer.',
+            'answer_text.min' => 'Your answer must be at least 5 characters long.'
+        ]);
+        $question_id = $request->input('question_id');
+        $answer = new Answer();
+        $answer->answer_text = $request->input('answer_text');
+        $answer->question_id = $question_id;
+        $answer->save();
+        return redirect("/questions/$question_id");
+    }
 }
--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
+use App\Modes\Answer;
 
 class QuestionsController extends Controller
 {
-   
+    public function index()
+       {
+            $questions = Question::reorder('id','desc')->get();
+            
+            return view('questions/index',['questions'=>$questions]);
+       }
+       
+       public function add(Request $request)
+       {
+          $request->validateWithBag('question',[
+               'statement' => 'required|min:5|ends_with:?'
+          ],[
+               'statement.required' => 'You must write something to submit a question.',
+               'statement.min' => 'Your question must be at least 5 characters long.',
+               'statement.ends_with' => 'Your question must finish with a question mark (?).'
+          ]);
+               $question = new Question();
+               $question->statement = $request->input('statement');
+               $question->save();
+               return redirect('/');
+       }
+
+       public function question(Request $request, $question_id)
+       {
+            $question = Question::find($question_id);
+            $answers = Answer::where('question_id',$question_id)->orderBy('id')->get();
+            return view('questions/question',['question'=>$question,'answers'=>$answers]);
+       }
+
 }
