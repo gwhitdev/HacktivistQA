f336e6d1fe1b7724013a520ab30da2ada224222e
Gareth Whitley <me@garethwhitley.online>
2021-07-08 13:51:27 +0100

    Added JS for random quotes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+const questionInputPlaceholder = document.getElementById('question');
+
+const placeHolderQuestions = [
+    'What is your quest?',
+    'What is your favourite colour?',
+    'What is the airspeed velocity of an unladen swallow?',
+];
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 3);
+}
+
+function chooseRandomQuestion(questionArray){
+    const number = getRandomNumber();
+    return questionArray[number];
+}
+
+questionInputPlaceholder.placeholder = chooseRandomQuestion(placeHolderQuestions);
