a28ace92bcb41935ffe5d738d36ef387776257ec
Gareth Whitley <me@garethwhitley.online>
2021-07-09 08:36:09 +0100

    moved question validation into own function in QuestionsController

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,32 +8,37 @@ use App\Models\Answer;
 
 class QuestionsController extends Controller
 {
-    public function index()
+     public function index()
        {
             $questions = Question::reorder('id','desc')->get();
             return view('questions/index',['questions'=>$questions]);
        }
        
-       public function add(Request $request)
-       {
-          $request->validateWithBag('question',[
+     public function validateQuestion(Request $request)
+     {
+          $validated = $request->validateWithBag('question',[
                'statement' => 'required|min:5|ends_with:?'
           ],[
                'statement.required' => 'You must write something to submit a question.',
                'statement.min' => 'Your question must be at least 5 characters long.',
                'statement.ends_with' => 'Your question must finish with a question mark (?).'
           ]);
-               $question = new Question();
-               $question->statement = $request->input('statement');
-               $question->save();
-               return redirect('/');
+          return $validated;
+     }
+     public function add(Request $request)
+       {
+          $this->validateQuestion($request);
+          $question = new Question();
+          $question->statement = $request->input('statement');
+          $question->save();
+          return redirect('/');
        }
 
-       public function question(Request $request, $question_id)
+     public function question(Request $request, $question_id)
        {
-            $question = Question::find($question_id);
-            $answers = Answer::where('question_id',$question_id)->orderBy('id')->get();
-            return view('questions/question',['question'=>$question,'answers'=>$answers]);
+          $question = Question::find($question_id);
+          $answers = Answer::where('question_id',$question_id)->orderBy('id')->get();
+          return view('questions/question',['question'=>$question,'answers'=>$answers]);
        }
 
 }
